3c1c02ff4fa7edba6009db44e17af5cd6103daa5
UdayaKiran442 <gudaya2002@email.com>
2025-12-31 19:01:33 +0530

    dvc final

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from src.entity import TrainingPipelineEntity
 from src.entity.input_entity.data_transformation_entity import DataTransformationEntity
+from src.entity.input_entity.data_ingestion_entity import DataIngestionEntity
 from src.entity.artifact_entity import DataIngestionArtifactEntity, DataTransformationArtifactEntity
 
 from src.utils.utils import clean_df, process_comment, get_transformation_object, save_numpy_array_data, save_object
@@ -64,16 +65,24 @@ class DataTransformation:
             raise NetworkSecurityException(e, sys)
         
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     training_pipeline_entity = TrainingPipelineEntity()
-    # Assuming DataIngestionEntity and DataIngestion are defined and work as expected
-    from src.entity.input_entity.data_ingestion_entity import DataIngestionEntity
-    from src.components.data_ingestion import DataIngestion
-
     data_ingestion_entity = DataIngestionEntity(training_pipeline_entity)
-    data_ingestion = DataIngestion(data_ingestion_entity)
-    data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-
+    
+    # Create DataIngestionArtifactEntity from existing files (from data_ingestion stage)
+    data_ingestion_artifact = DataIngestionArtifactEntity(
+        train_file_path=data_ingestion_entity.ingested_dir_train_file_path,
+        test_file_path=data_ingestion_entity.ingested_dir_test_file_path
+    )
+    
+    # Create data transformation entity
     data_transformation_entity = DataTransformationEntity(training_pipeline_entity)
+    
+    # Run data transformation
     data_transformation = DataTransformation(data_transformation_entity, data_ingestion_artifact)
     data_transformation_artifact = data_transformation.initiate_data_transformation()
+    
+    print(f"Data transformation completed!")
+    print(f"Train file: {data_transformation_artifact.data_transformation_train_file_path}")
+    print(f"Test file: {data_transformation_artifact.data_transformation_test_file_path}")
+    print(f"Transformation object: {data_transformation_artifact.data_transformation_object_file_path}")
--- src/entity/input_entity/data_transformation_entity.py
+++ src/entity/input_entity/data_transformation_entity.py
@@ -8,6 +8,6 @@ from src.constants.constants import DATA_TRANSFORMATION_DIR, DATA_TRANSFORMATION
 class DataTransformationEntity:
     def __init__(self, training_pipeline_entity: TrainingPipelineEntity):
         self.data_transformation_dir = os.path.join(training_pipeline_entity.artifact_dir, DATA_TRANSFORMATION_DIR)
-        self.data_transformation_train_file_path = os.path.join(self.data_transformation_dir, DATA_TRANSFORMATION_TRAIN_FILE_PATH)
-        self.data_transformation_test_file_path = os.path.join(self.data_transformation_dir, DATA_TRANSFORMATION_TEST_FILE_PATH)
-        self.data_transformation_object_file_path = os.path.join(self.data_transformation_dir, DATA_TRANSFORMATION_OBJECT_FILE_PATH)
+        self.data_transformation_train_file_path = os.path.join(self.data_transformation_dir, "transformed_data", DATA_TRANSFORMATION_TRAIN_FILE_PATH)
+        self.data_transformation_test_file_path = os.path.join(self.data_transformation_dir, "transformed_data", DATA_TRANSFORMATION_TEST_FILE_PATH)
+        self.data_transformation_object_file_path = os.path.join(self.data_transformation_dir, "transformed_object", DATA_TRANSFORMATION_OBJECT_FILE_PATH)
