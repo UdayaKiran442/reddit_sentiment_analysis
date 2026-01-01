27ce6b1429fa8373853a3f3b0fd72cd0edfc41c0
UdayaKiran442 <gudaya2002@email.com>
2026-01-01 10:59:46 +0530

    model trainer dvc stage

--- src/entity/input_entity/model_trainer_entity.py
+++ src/entity/input_entity/model_trainer_entity.py
@@ -7,4 +7,4 @@ from src.constants.constants import  MODEL_TRAINER_DIR, MODEL_TRAINER_TRAINED_MO
 class ModelTrainerEntity:
     def __init__(self, training_pipeline_entity: TrainingPipelineEntity):
         self.model_trainer_dir = os.path.join(training_pipeline_entity.artifact_dir, MODEL_TRAINER_DIR)
-        self.trained_model_file_path = os.path.join(self.model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_FILE_PATH)
+        self.trained_model_file_path = os.path.join(self.model_trainer_dir, "model", MODEL_TRAINER_TRAINED_MODEL_FILE_PATH)
